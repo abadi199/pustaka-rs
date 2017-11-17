db76d8734453adea538d6851400b27eefc0bb1f2
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2017-11-16 18:12:11 -0600

    Add more seeds

--- src/server/bin/seed.rs
+++ src/server/bin/seed.rs
@@ -17,42 +17,56 @@ fn main() {
 fn category(connection: &SqliteConnection) {
     use schema::category::dsl::category;
 
-    diesel::delete(category)
-        .execute(&*connection)
-        .expect("Error deleting categories");
-    diesel::insert(&NewCategory {
-        name: "Fiction".to_string(),
-        parent_id: None,
-    }).into(category)
-        .execute(&*connection)
-        .expect("Error inserting category");
+    diesel::delete(category).execute(&*connection).expect(
+        "Error deleting categories",
+    );
+
+    let categories = vec!["Fiction".to_string(), "Non Fiction".to_string()];
+    for name in categories {
+        diesel::insert(&NewCategory {
+            name: name,
+            parent_id: None,
+        }).into(category)
+            .execute(&*connection)
+            .expect("Error inserting category");
+    }
 }
 
 fn media_type(connection: &SqliteConnection) {
     use schema::media_type::dsl::media_type;
 
-    diesel::delete(media_type)
-        .execute(&*connection)
-        .expect("Error deleting media_types");
-    diesel::insert(&NewMediaType {
-        name: "ebook".to_string(),
-    }).into(media_type)
-        .execute(&*connection)
-        .expect("Error inserting media_type");
+    diesel::delete(media_type).execute(&*connection).expect(
+        "Error deleting media_types",
+    );
+    let media_types = vec![
+        "book".to_string(),
+        "comic".to_string(),
+        "magazine".to_string(),
+        "audiobook".to_string(),
+        "manga".to_string(),
+        "hardcopy".to_string(),
+    ];
+    for name in media_types {
+        diesel::insert(&NewMediaType { name: name })
+            .into(media_type)
+            .execute(&*connection)
+            .expect("Error inserting media_type");
+    }
+
 }
 
 fn author(connection: &SqliteConnection) {
     use schema::author::dsl::author;
 
-    diesel::delete(author)
-        .execute(&*connection)
-        .expect("Error deleting authors");
+    diesel::delete(author).execute(&*connection).expect(
+        "Error deleting authors",
+    );
 }
 
 fn publication(connection: &SqliteConnection) {
     use schema::publication::dsl::publication;
 
-    diesel::delete(publication)
-        .execute(&*connection)
-        .expect("Error deleting publication");
+    diesel::delete(publication).execute(&*connection).expect(
+        "Error deleting publication",
+    );
 }
