2d120d56433cfa1e20da0c6220f8a9ac61fe63d4
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2018-06-07 17:07:41 -0500

    Add more categories to seed

--- src/server/seed.rs
+++ src/server/seed.rs
@@ -15,18 +15,84 @@ fn main() {
 }
 
 fn category(connection: &SqliteConnection) {
-    use schema::category::dsl::category;
+    use schema::category::dsl::*;
 
     diesel::delete(category)
         .execute(&*connection)
         .expect("Error deleting categories");
 
     let categories = vec!["Fiction".to_string(), "Non Fiction".to_string()];
-    for name in categories {
+    for category_name in categories {
         diesel::insert_into(category)
             .values(&NewCategory {
-                name: name,
+                name: category_name,
                 parent_id: None,
+            })
+            .execute(connection)
+            .expect("Error inserting category");
+    }
+
+    let fiction: Category = category
+        .filter(name.eq("Fiction"))
+        .limit(1)
+        .load(connection)
+        .expect("Error finding Fiction id")
+        .remove(0);
+
+    let fictions = vec![
+        "Classic".to_string(),
+        "Comics/Graphic novel".to_string(),
+        "Crime/detective".to_string(),
+        "Fable".to_string(),
+        "Fairy tale".to_string(),
+        "Fan fiction".to_string(),
+        "Fantasy".to_string(),
+        "Folklore".to_string(),
+        "Historical fiction".to_string(),
+        "Horror".to_string(),
+        "Humor".to_string(),
+        "Legend".to_string(),
+        "Magical realism".to_string(),
+        "Meta fiction".to_string(),
+        "Mystery".to_string(),
+        "Mythology".to_string(),
+        "Mythopoeia".to_string(),
+        "Picture book".to_string(),
+        "Realistic fiction".to_string(),
+        "Science fiction".to_string(),
+        "Short story".to_string(),
+        "Suspense/thriller".to_string(),
+        "Tall tale".to_string(),
+        "Western".to_string(),
+    ];
+    for category_name in fictions {
+        diesel::insert_into(category)
+            .values(&NewCategory {
+                name: category_name,
+                parent_id: Some(fiction.id),
+            })
+            .execute(connection)
+            .expect("Error inserting category");
+    }
+
+    let non_fictions = vec![
+        "Biography".to_string(),
+        "Essay".to_string(),
+        "Owner's Manual".to_string(),
+        "Journalism".to_string(),
+        "Lab Report".to_string(),
+        "Memoir".to_string(),
+        "Narrative nonfiction".to_string(),
+        "Reference book".to_string(),
+        "Self-help book".to_string(),
+        "Speech".to_string(),
+        "Textbook".to_string(),
+    ];
+    for category_name in non_fictions {
+        diesel::insert_into(category)
+            .values(&NewCategory {
+                name: category_name,
+                parent_id: Some(fiction.id),
             })
             .execute(connection)
             .expect("Error inserting category");
