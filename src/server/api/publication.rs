dcdf2f2fbaabc3f7d3ba2f8ac9f1cedf714e6aad
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2018-10-08 08:29:02 -0500

    Implement publication CRUD.

--- src/server/api/publication.rs
+++ src/server/api/publication.rs
@@ -1,10 +1,10 @@
 extern crate diesel;
 
 use actix_web::http::Method;
-use actix_web::{middleware, App, AsyncResponder, FutureResponse, HttpResponse, Path, State};
+use actix_web::{middleware, App, AsyncResponder, FutureResponse, HttpResponse, Json, Path, State};
 use db::publication::{Create, Delete, Get, List, Update};
 use futures::Future;
-use models::{Category, NewCategory};
+use models::{NewPublication, Publication};
 use state::AppState;
 
 fn list(state: State<AppState>) -> FutureResponse<HttpResponse> {
@@ -13,11 +13,58 @@ fn list(state: State<AppState>) -> FutureResponse<HttpResponse> {
         .send(List {})
         .from_err()
         .and_then(|res| match res {
-            Ok(categories) => Ok(HttpResponse::Ok().json(categories)),
+            Ok(publications) => Ok(HttpResponse::Ok().json(publications)),
             Err(_) => Ok(HttpResponse::InternalServerError().into()),
         }).responder()
 }
 
+fn create(state: State<AppState>, json: Json<NewPublication>) -> FutureResponse<HttpResponse> {
+    state
+        .db
+        .send(Create {
+            new_publication: json.into_inner(),
+        }).from_err()
+        .and_then(|res| match res {
+            Ok(_) => Ok(HttpResponse::Ok().json(())),
+            Err(_) => Ok(HttpResponse::InternalServerError().into()),
+        }).responder()
+}
+
+fn update(state: State<AppState>, json: Json<Publication>) -> FutureResponse<HttpResponse> {
+    state
+        .db
+        .send(Update {
+            publication: json.into_inner(),
+        }).from_err()
+        .and_then(|res| match res {
+            Ok(_) => Ok(HttpResponse::Ok().json(())),
+            Err(_) => Ok(HttpResponse::InternalServerError().into()),
+        }).responder()
+}
+
+fn delete(state: State<AppState>, publication_id: Path<i32>) -> FutureResponse<HttpResponse> {
+    state
+        .db
+        .send(Delete {
+            publication_id: publication_id.into_inner(),
+        }).from_err()
+        .and_then(|res| match res {
+            Ok(_) => Ok(HttpResponse::Ok().json(())),
+            Err(err) => Ok(HttpResponse::InternalServerError().into()),
+        }).responder()
+}
+
+fn get(state: State<AppState>, publication_id: Path<i32>) -> FutureResponse<HttpResponse> {
+    state
+        .db
+        .send(Get {
+            publication_id: publication_id.into_inner(),
+        }).from_err()
+        .and_then(|res| match res {
+            Ok(publication) => Ok(HttpResponse::Ok().json(publication)),
+            Err(_) => Ok(HttpResponse::InternalServerError().into()),
+        }).responder()
+}
 // #[get("/read/<the_publication_id>")]
 // fn read(the_publication_id: i32, connection: DbConn) -> Json<reader::Data> {
 //     use schema::publication::dsl as publication;
@@ -44,32 +91,20 @@ fn list(state: State<AppState>) -> FutureResponse<HttpResponse> {
 //     NamedFile::open(filename).ok()
 // }
 
-fn get(state: State<AppState>, publication_id: Path<i32>) -> FutureResponse<HttpResponse> {
-    state
-        .db
-        .send(Get {
-            publication_id: publication_id.into_inner(),
-        }).from_err()
-        .and_then(|res| match res {
-            Ok(publication) => Ok(HttpResponse::Ok().json(publication)),
-            Err(_) => Ok(HttpResponse::InternalServerError().into()),
-        }).responder()
-}
-
-// #[get("/category/<the_category_id>")]
-// fn by_category(the_category_id: i32, connection: DbConn) -> Json<Vec<Publication>> {
+// #[get("/publication/<the_publication_id>")]
+// fn by_publication(the_publication_id: i32, connection: DbConn) -> Json<Vec<Publication>> {
 //     use schema::publication::dsl as publication;
-//     use schema::publication_category::dsl as publication_category;
+//     use schema::publication_publication::dsl as publication_publication;
 
-//     let categories: Vec<i32> = get_category_and_descendants(the_category_id, &connection)
-//         .expect("Invalid category id")
+//     let publications: Vec<i32> = get_publication_and_descendants(the_publication_id, &connection)
+//         .expect("Invalid publication id")
 //         .iter()
-//         .map(|category| category.id)
+//         .map(|publication| publication.id)
 //         .collect();
 
-//     let the_publication_id = publication_category::publication_category
-//         .filter(publication_category::category_id.eq_any(categories))
-//         .select(publication_category::publication_id)
+//     let the_publication_id = publication_publication::publication_publication
+//         .filter(publication_publication::publication_id.eq_any(publications))
+//         .select(publication_publication::publication_id)
 //         .load::<i32>(&*connection)
 //         .expect("Error getting publications id");
 
@@ -94,40 +129,40 @@ fn get(state: State<AppState>, publication_id: Path<i32>) -> FutureResponse<Http
 //         .and_then(|tn| NamedFile::open(tn).ok())
 // }
 
-// fn get_category(category_id: i32, connection: &SqliteConnection) -> QueryResult<Category> {
-//     use schema::category::dsl as category;
+// fn get_publication(publication_id: i32, connection: &SqliteConnection) -> QueryResult<Publication> {
+//     use schema::publication::dsl as publication;
 
-//     category::category
-//         .filter(category::id.eq(category_id))
-//         .first::<Category>(&*connection)
+//     publication::publication
+//         .filter(publication::id.eq(publication_id))
+//         .first::<Publication>(&*connection)
 // }
 
-// fn get_category_and_descendants(
-//     category_id: i32,
+// fn get_publication_and_descendants(
+//     publication_id: i32,
 //     connection: &SqliteConnection,
-// ) -> QueryResult<Vec<Category>> {
-//     let mut categories: Vec<Category> = vec![];
+// ) -> QueryResult<Vec<Publication>> {
+//     let mut publications: Vec<Publication> = vec![];
 
-//     let parent_category = get_category(category_id, connection)?;
-//     categories.push(parent_category);
+//     let parent_publication = get_publication(publication_id, connection)?;
+//     publications.push(parent_publication);
 
-//     let mut children = get_descendant_rec(category_id, connection)?;
-//     categories.append(&mut children);
+//     let mut children = get_descendant_rec(publication_id, connection)?;
+//     publications.append(&mut children);
 
-//     Ok(categories)
+//     Ok(publications)
 // }
 
 // fn get_descendant_rec(
-//     category_id: i32,
+//     publication_id: i32,
 //     connection: &SqliteConnection,
-// ) -> QueryResult<Vec<Category>> {
-//     use schema::category::dsl as category;
-//     let mut categories = category::category
-//         .filter(category::parent_id.eq(category_id))
-//         .load::<Category>(connection)?;
+// ) -> QueryResult<Vec<Publication>> {
+//     use schema::publication::dsl as publication;
+//     let mut publications = publication::publication
+//         .filter(publication::parent_id.eq(publication_id))
+//         .load::<Publication>(connection)?;
 
-//     let mut grandchildren: Vec<Category> = vec![];
-//     for c in categories.iter() {
+//     let mut grandchildren: Vec<Publication> = vec![];
+//     for c in publications.iter() {
 //         let mut result = get_descendant_rec(c.id, connection);
 //         match result {
 //             Ok(mut d) => {
@@ -137,49 +172,49 @@ fn get(state: State<AppState>, publication_id: Path<i32>) -> FutureResponse<Http
 //         }
 //     }
 
-//     categories.append(&mut grandchildren);
-//     Ok(categories)
+//     publications.append(&mut grandchildren);
+//     Ok(publications)
 // }
 
 // END HERE
 
 // #[post("/", data = "<json>")]
-// fn create(json: Json<NewCategory>, connection: DbConn) {
-//     let new_category = &json.0;
-//     diesel::insert_into(category)
-//         .values(new_category)
+// fn create(json: Json<NewPublication>, connection: DbConn) {
+//     let new_publication = &json.0;
+//     diesel::insert_into(publication)
+//         .values(new_publication)
 //         .execute(&*connection)
-//         .expect("Error inserting category");
+//         .expect("Error inserting publication");
 // }
 
 // #[put("/", data = "<json>")]
-// fn update(json: Json<Category>, connection: DbConn) {
-//     let the_category = &json.0;
-//     diesel::update(category.filter(id.eq(the_category.id)))
-//         .set(the_category)
+// fn update(json: Json<Publication>, connection: DbConn) {
+//     let the_publication = &json.0;
+//     diesel::update(publication.filter(id.eq(the_publication.id)))
+//         .set(the_publication)
 //         .execute(&*connection)
-//         .expect("Error updating category");
+//         .expect("Error updating publication");
 // }
 
-// #[delete("/<category_id>")]
-// fn delete(category_id: i32, connection: DbConn) {
-//     diesel::delete(category.filter(id.eq(category_id)))
+// #[delete("/<publication_id>")]
+// fn delete(publication_id: i32, connection: DbConn) {
+//     diesel::delete(publication.filter(id.eq(publication_id)))
 //         .execute(&*connection)
-//         .expect(&format!("Error deleting category {}", category_id));
+//         .expect(&format!("Error deleting publication {}", publication_id));
 // }
 
-// #[get("/<category_id>")]
-// fn get(category_id: i32, connection: DbConn) -> Json<Category> {
-//     let mut row = category
-//         .filter(id.eq(category_id))
+// #[get("/<publication_id>")]
+// fn get(publication_id: i32, connection: DbConn) -> Json<Publication> {
+//     let mut row = publication
+//         .filter(id.eq(publication_id))
 //         .limit(1)
 //         .load(&*connection)
-//         .expect(&format!("Error loading category with id {}", category_id));
+//         .expect(&format!("Error loading publication with id {}", publication_id));
 
 //     match row.is_empty() {
 //         true => panic!(format!(
-//             "category with id of {} can't be found",
-//             category_id
+//             "publication with id of {} can't be found",
+//             publication_id
 //         )),
 //         false => Json(row.remove(0)),
 //     }
@@ -192,8 +227,11 @@ pub fn create_app(state: AppState, prefix: &str) -> App<AppState> {
         // .route("/", Method::GET, read)
         // .route("/", Method::GET, read_page)
         .route("/", Method::GET, list)
-        .route("/{publication_id}", Method::GET, get)
-    // .route("/", Method::GET, by_category)
+        .route("/", Method::POST, create)
+        .route("/", Method::PUT, update)
+        .route("/{publication_id}", Method::DELETE, delete)
+         .route("/{publication_id}", Method::GET, get)
+    // .route("/", Method::GET, by_publication)
     // .route("/", Method::GET, get_thumbnail)
     // .route("/", Method::GET, get_publication)
 }
