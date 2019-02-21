7bff82a9c8797fe459b3a7fb2ba1528a9aa45107
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2019-02-20 23:13:39 -0600

    Forgot to add file

---
+++ src/server/scan/actor/load_metadata.rs
@@ -0,0 +1,35 @@
+use actix::prelude::*;
+use actix_web::client;
+use models::{CategoryId, NewPublication};
+use scan::actor::{File, Scanner};
+use scan::error::ScannerError;
+
+pub struct LoadMetadata {
+    pub file: File,
+    pub category_id: CategoryId,
+}
+
+impl Message for LoadMetadata {
+    type Result = Result<(NewPublication, CategoryId), ScannerError>;
+}
+
+impl Handler<LoadMetadata> for Scanner {
+    type Result = Result<(NewPublication, CategoryId), ScannerError>;
+
+    fn handle(&mut self, msg: LoadMetadata, _: &mut Self::Context) -> Self::Result {
+        let file = msg.file;
+        let category_id = msg.category_id;
+        let publication = NewPublication {
+            isbn: "".to_string(),
+            title: file.name.clone(),
+            media_type_id: 1,
+            media_format: file.extension.clone(),
+            author_id: 1,
+            thumbnail: None,
+            file: file.name.clone(),
+        };
+        println!("{:?}", publication);
+
+        Ok((publication, category_id))
+    }
+}
